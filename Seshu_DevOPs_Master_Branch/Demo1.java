dac0ff1993ed00ce644528f502e6b21ed2899922
sgavara <babu.seshu@gmail.com>
2018-07-17 06:24:48 +0530

    Future branch files

---
+++ Seshu_DevOPs_Master_Branch/Demo1.java
@@ -0,0 +1,13 @@
+public class Demo1
+{
+Demo1(){}
+
+public void testDemo(){
+ System.out.println("Demo Test1");
+}
+
+public static void main(String args[]){
+ Demo1 demo=new Demo1();
+demo.testDemo();
+}
+}
